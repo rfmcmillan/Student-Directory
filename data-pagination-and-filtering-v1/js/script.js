6df07ceb696abc13d26bb1d682c796cd1fbdf827
Russel McMillan <rfmcmillan@gmail.com>
2020-10-24 07:04:33 -0400

    JS almost complete - just a few bugs left to fix

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -3,16 +3,6 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
@@ -24,15 +14,15 @@ function showPage (list, page) {
    let startIdx = (page * itemsPerPage) - itemsPerPage;
    // create endIdx variable = page parameter * items per page
    let endIdx = (page * itemsPerPage);
-   // create new variable and assign the UL element with a class of 'student-list' to it
+   // create new variable and assign to it the UL element with a class of 'student-list'
    let studentList = document.querySelector('.student-list');
-   // set the innerHTML of the above variable to empty string to wipe out previously displayed students at page reload
+   // set the innerHTML of the above variable to an empty string to remove the previously displayed students from being displayed at page reload
    studentList.innerHTML = '';
-   // for loop over the list parameter
+   // for loop that loops over the list that is passed in as an argument
    for (let i = 0; i < list.length; i++) {
-   //    if statement: check if the i is greater or less than startIdx and EndIdx
+   //    if statement: check if the i is greater than startIdx and less than EndIdx
       if (i >= startIdx && i < endIdx) {
-   //       run the below insertStudent helper function
+   //       run the insertStudent helper function that is defined below
          let currStudent = list[i]; 
          insertStudent(currStudent)
       // end if
@@ -40,8 +30,7 @@ function showPage (list, page) {
    // end for
    }
 
-
-   // Create the insertStudent helper function 
+   // Create the insertStudent helper function that will be included in the showPage function.
    function insertStudent (student) {
    // create li and give a className of "student-item cf"
       const studentLI = document.createElement('li');
@@ -53,14 +42,14 @@ function showPage (list, page) {
       studentDetails.className = 'student-details';
    // append the div in the li
       studentLI.appendChild(studentDetails);
-   // create an img and give className of "avatar", give a src and alt as well
+   // create an img and give className of "avatar", give it a src and alt as well
       const studentImage = document.createElement('img');
       studentImage.className = 'avatar';
       studentImage.src = student.picture.medium
       studentImage.alt = 'Picture of Student'
    // append img in the div
       studentDetails.appendChild(studentImage);
-   // create h3 and change text Content to a student name
+   // create h3 and change text content to be a student name
       const studentName = document.createElement('h3');
       studentName.textContent = `${student.name.title} ${student.name.first} ${student.name.last}`
    // append to the div
@@ -87,20 +76,22 @@ function showPage (list, page) {
       joinedDate.appendChild(joinedSpan);
    }
 }
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-// function accepts single list parameter
+// function accepts an array as the only argument
 function addPagination (list) {
-   //create variable to store number of pagination buttons needed
+   // create variable to hold the number of students you want to display
    const itemsPerPage = 9;
+   //create variable to store number of pagination buttons needed
    const numberOfButtons = Math.ceil(data.length/itemsPerPage);
    // create variable to store UL with classname 'link-list'
    const paginationList = document.querySelector('.link-list');
    // set innerHTML to empty string to clear out any previous pagination buttons
    paginationList.innerHTML = '';
-   // for loop over the variable for the number of pagination buttons
+   // for loop that creates the pagination buttons
    for (let i = 0; i < numberOfButtons; i++) {
    //    create li
       const buttonLI = document.createElement('li');
@@ -117,18 +108,28 @@ function addPagination (list) {
    // end for
    }
    // select first button
-   
+   let firstButton = document.querySelector('button');
    // give className of 'active'
-   
-   // create eventListener to listen for clicks on the link-list variable
+   firstButton.className = 'active';
+   // create eventListener to listen for clicks on any of the pagination buttons
+   paginationList.addEventListener('click', (e) => {
       // if click target is a pagination button
-         // remove 'active' class from any other pagination button
+      if (e.target.tagName = 'button') {
+         // for loop that loops through all the buttons 
+         for (let i = 0; i < paginationList.children.length; i++) {
+            // removes 'acive' className from all buttons
+            paginationList.children[i].firstElementChild.className = ''
+         // end for
+         }
          // add 'active' class to click target
-         // call showPage function passing 'data' as the list argument and the button's text content as the page argument
+         e.target.className = 'active';
+         // to show the selected page, call the showPage function passing 'data' as the list argument and the button's corresponding page number as the page argument
+         showPage(data,e.target.textContent)
+      // end if
+      }
+   });
 }
 
-
-
-// Call functions
+// Call functions for the initial page load. These load the students and the page buttons. 
 showPage(data, 1)
 addPagination(data);
